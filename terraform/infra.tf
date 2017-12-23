529528e9e5ae9c439cd37e7b98179bdc967f0782
Bob Clarke <bobmclarke@gmail.com>
2017-12-23 15:55:49 +0000

    Adding kubernetes controller and node to infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -10,13 +10,16 @@ provider "aws" {
 resource "aws_vpc" "kube" {
   cidr_block = "10.0.0.0/16"
   tags 	{
-    Name = "christmas-2017"
+    Name = "kube-vpc"
   }
 }
 
 # Create an internet gateway to give our subnet access to the outside world
 resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.kube.id}"
+  tags 	{
+    Name = "kube-int-gw"
+  }
 }
 
 # Grant the VPC internet access on its main route table
@@ -32,6 +35,9 @@ resource "aws_subnet" "subnet_1" {
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
+  tags 	{
+    Name = "kube-subnet-1"
+  }
 }
 
 # Our default security group to access our instances over SSH and HTTP
@@ -55,6 +61,25 @@ resource "aws_security_group" "default" {
   }
 }
 
+resource "aws_instance" "ansible_server" {
+  ami           = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  subnet_id = "${aws_subnet.subnet_1.id}"
+  availability_zone = "us-east-1a"
+
+  provisioner "file" {
+    source      = "install_ansible.sh"
+    destination = "/tmp/script.sh"
+  }
+
+  tags {
+    Name = "ansible-server"
+  }
+}
+
+
 resource "aws_instance" "kube_controller" {
   ami           = "${lookup(var.aws_amis, var.aws_region)}"
   instance_type = "${var.instance_type}"
@@ -63,7 +88,43 @@ resource "aws_instance" "kube_controller" {
   subnet_id = "${aws_subnet.subnet_1.id}"
   availability_zone = "us-east-1a"
   tags {
-    Name = "kube_controller"
+    Name = "kube-controller"
+  }
+}
+
+resource "aws_instance" "kube_node_1" {
+  ami           = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  subnet_id = "${aws_subnet.subnet_1.id}"
+  availability_zone = "us-east-1a"
+  tags {
+    Name = "kube-node-1"
+  }
+}
+
+resource "aws_instance" "kube_node_2" {
+  ami           = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  subnet_id = "${aws_subnet.subnet_1.id}"
+  availability_zone = "us-east-1a"
+  tags {
+    Name = "kube-node-2"
+  }
+}
+
+resource "aws_instance" "kube_node_3" {
+  ami           = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  subnet_id = "${aws_subnet.subnet_1.id}"
+  availability_zone = "us-east-1a"
+  tags {
+    Name = "kube-node-3"
   }
 }
 
