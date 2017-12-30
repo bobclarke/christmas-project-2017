9111ec7e85142cf894675f5dfc48d6be4ab53b0b
Bob Clarke <bobmclarke@gmail.com>
2017-12-30 00:15:07 +0000

    Updating common_setup.sh

--- terraform/common_setup.sh
+++ terraform/common_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Remove stuff we don't need
+# Remove packages we don't need (we'll install the appropriate version of docker sing ansible)
 sudo yum remove \
   docker \
   docker-common \
@@ -9,35 +9,33 @@ sudo yum remove \
   docker-engine \
   docker-ce
 
-# Setup yum
-#sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-#sudo yum-config-manager --enable docker-ce-edge
-#sudo yum-config-manager --enable docker-ce-test
-#sudo yum-config-manager --disable docker-ce-edge
-
 # Install packages
-sudo yum install -y \
-  git \
-  ansible \
+sudo yum install -y git ansible 
 
 sleep 3
 
-# Populate hosts file
+# Populate hosts file with AWS internal IPs
 sudo bash -c "cat >> /etc/hosts" << "EOF"
-10.0.1.100  ansible
-10.0.1.101  kube_controller
-10.0.1.111  kube_node_1
-10.0.1.112  kube_node_2
-10.0.1.113  kube_node_3
+10.0.1.100  ansible ip-10-0-1-100
+10.0.1.101  kube_controller ip-10-0-1-101
+10.0.1.111  kube_node_1 ip-10-0-1-111
+10.0.1.112  kube_node_2 ip-10-0-1-112
+10.0.1.113  kube_node_3 ip-10-0-1-113
 EOF
 
 sleep 3
 
-# Create ansible account, set password, add ssh keys and setup sudoers
+# Create ansible account
 sudo useradd -m -u 30000 ansible
+
+# Set ansible password
 echo -e "ansible\nansible\n" | sudo passwd ansible
+
+# Set up sudoers for ansible account
 sudo bash -c "cat > /etc/sudoers.d/10_ansible" << "EOF"
 ansible ALL=(ALL) NOPASSWD: ALL
 EOF
 
-
+# Todo 
+# Generate ssh keypair and distribute public key 
+# Invoke ansible auptmatically (maybe as a 2nd provisioner for the ansible master)
